9e9f7a0204ffb0c6f64e2fa7827c8242fd6414f1
零华 <3262064245@qq.com>
2023-05-03 09:01:32 +0800

    更新

---
+++ 1.C#单例模式/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_单例模式
+{
+    internal class SingleOne
+    {
+        // 定义一个静态变量来保存类的实例
+        private static SingleOne instance;
+
+        private int x;
+        private int y;
+        private int z;
+
+        //定义一个私有构造函数，使外界不能对它操作
+        private SingleOne()
+        {
+            x = 1; y = 2; z = 3;
+        }
+
+        //这是一个简单的单例模式
+        //如果这是单线程，那么就到此为止了
+        public static SingleOne GetSingleOne1()
+        {
+            if (instance == null)
+            {
+                instance = new SingleOne();
+            }
+            return instance;
+        }
+
+        //对于多线程的限制访问
+        private static object locker = new object();
+
+        public static SingleOne GetSingleOne2()
+        {
+            //通过加锁防止重复创建实例
+            lock (locker)
+            {
+                if (instance == null)
+                {
+                    instance = new SingleOne();
+                }
+            }
+            return instance;
+        }
+
+        //优化
+        public static SingleOne GetSingleOne3()
+        {
+            //我们加锁是为了防止重复创建实例
+            //那么其实只需要判断是否为空，然后再锁
+            //如果不为空，也就不需要在锁外进行判断
+            if (instance == null)
+            {
+                //加锁是为了防止某些线程同时判断为空进入
+                lock (locker)
+                {
+                    //再判断一下，如果前有前程创建了就不管了
+                    if (instance == null)
+                    {
+                        instance = new SingleOne();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SingleOne singleOne = SingleOne.GetSingleOne3();
+        }
+    }
+}
---
+++ 2.C#简单工厂模式/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _2.C_简单工程模式
+{
+    internal interface IMakingCars
+    {
+        //所有产品的抽象的同一制作流程
+        void GetCollar();
+
+        void Making();
+    }
+
+    //两个不同的产品的具体制作过程
+    internal class _RedCar : IMakingCars
+    {
+        public void Making()
+        { Console.WriteLine("this is Red car"); }
+
+        public void GetCollar()
+        { }
+    }
+
+    internal class _BlueCar : IMakingCars
+    {
+        public void Making()
+        { Console.WriteLine("this a Blue Car"); }
+
+        public void GetCollar()
+        { }
+    }
+
+    //创建一个产品工厂，负责为这些派生的产品创造实例
+    internal class CarFactory
+    {
+        public IMakingCars MakingCars(string collar)
+        {
+            switch (collar)
+            {
+                case "Blue": return new _BlueCar();//
+                case "Red": return new _RedCar();
+            }
+
+            return null;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CarFactory factory = new CarFactory();
+
+            //造出红色车
+            IMakingCars blueCar = factory.MakingCars("Blue");
+            IMakingCars redcars = factory.MakingCars("Red");
+
+            if (blueCar != null)
+            {
+                blueCar.Making();
+            }
+            if (redcars != null)
+            {
+                redcars.Making();
+            }
+        }
+    }
+}
