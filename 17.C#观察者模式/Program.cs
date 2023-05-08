c45b4ddb383b01ac9455aa2d022fc1322c56e6dc
零华 <3262064245@qq.com>
2023-05-08 23:03:57 +0800

    qqq

--- 17.C#观察者模式/Program.cs
+++ 17.C#观察者模式/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +24,7 @@ namespace _17.C_观察者模式
         }
 
         //接收函数
+        //传入公众号是想要观察者知道是哪个公众号更新了
         public abstract void Receive(Publisher publisher);
     }
     class Subscriber : Observer
@@ -65,7 +67,6 @@ namespace _17.C_观察者模式
             }
         }
     }
-
     class WeiXin : Publisher
     {
         public WeiXin(string name)
@@ -78,14 +79,79 @@ namespace _17.C_观察者模式
     {
         static void Main(string[] args)
         {
-            Observer observer = new Subscriber("zerohua","1111");
 
+            Observer observer = new Subscriber("zerohua", "1111");
             Publisher publisher = new WeiXin("微信运动");
-
             //让observer加入订阅
             publisher.AddObserver(observer);
             //更新，通知
             publisher.Update();
+
+            //采用委托的方式进行
+
         }
     }
+}
+namespace _以委托实现观察者模式
+{
+    //抽象订阅号
+    abstract class Publisher
+    {
+        //以委托代替列表
+        Action<Publisher> handler;
+        //初始化订阅号信息
+        public string Name { get; set; }
+        public Publisher(string Name) { this.Name = Name; }
+
+        //管理订阅
+        //增加订阅
+        public void Add(Action<Publisher> action)
+        {
+            handler += action;
+        }
+        //减少订阅
+        public void Delete(Action<Publisher> action)
+        {
+            handler -= action;
+        }
+        //订阅更新
+        public void Update()
+        {
+            handler(this);
+        }
+    }
+    class WeiXin : Publisher
+    {
+        public WeiXin(string Name) : base(Name)
+        {
+        }
+    }
+
+    //订阅者
+    class Subscriber
+    {
+        //订阅者信息
+        public string Name { get; set; }
+        public Subscriber(string name) { this.Name = name; }
+        //请求更新
+        public void Receive(Publisher publisher)
+        {
+            Console.WriteLine("用户" + Name + ",你订阅的" + publisher.Name + "更新了");
+        }
+    }
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Subscriber subscriber = new Subscriber("Tom");
+            Subscriber subscriber2 = new Subscriber("Lon");
+
+            Publisher publisher = new WeiXin("世华公众号");
+            publisher.Add(subscriber.Receive);
+            publisher.Add(subscriber2.Receive);
+            
+            publisher.Update();
+        }
+    }
+
 }
