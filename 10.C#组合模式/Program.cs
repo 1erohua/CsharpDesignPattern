b15ce46e9e4d7104cfe2d447472cd2df6dd4a037
零华 <3262064245@qq.com>
2023-05-11 12:18:28 +0800

    重审代码

--- 10.C#组合模式/Program.cs
+++ 10.C#组合模式/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace _10.C_组合模式
 {
+    //它允许将对象组合成树形结构以表示“整体/部分”层次结构。
+    //使用此模式，客户端可以按相同的方式处理单个对象和对象集合，而不必关注它们是单个对象还是组合对象
     abstract class WenJian
     {
         protected string fileName;
@@ -30,7 +32,7 @@ namespace _10.C_组合模式
     }
 
 
-    class File : WenJian
+    class File : WenJian// 组合树结构中的最底层元素，它们没有子节点，具有特定的行为。
     {
         public File(string name) : base(name)
         {
@@ -61,13 +63,13 @@ namespace _10.C_组合模式
         }
     }
 
-    class FileFolder : WenJian
+    class FileFolder : WenJian//容器: 包含一组子节点并维护它们之间的组合结构。容器可以包含其他容器和叶子节点，统一对子节点操作
     {
         public FileFolder(string name):base(name)
         {
 
         }
-        private List<WenJian> files=new List<WenJian>();
+        private List<WenJian> files=new List<WenJian>();//复杂对象由简单对象组成
         public override void Add(WenJian wenJian)
         {
             files.Add(wenJian);
